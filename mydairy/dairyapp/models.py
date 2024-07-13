78cf73e89f30e32eaf9141581e8592611e5fa12f
prabuddha chunchekar <prabuddhachunchekar@gmail.com>
2024-07-13 22:28:58 +0530

    foreign_key_added

--- mydairy/dairyapp/models.py
+++ mydairy/dairyapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class DairyEntry(models.Model):
     title  = models.CharField(max_length=200)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    uid = models.ForeignKey(User, on_delete=models.CASCADE, db_column="uid")
